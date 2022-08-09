a6a3da8815320f0d1bc591f40f075212d0464a63
buncho315 <mzh.hrtk125@gmail.com>
2022-08-09 02:39:26 +0000

    homes & about Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  root to: "homes#top"
+  get "/about" => 'homes#about', as: 'about'
   devise_for :end_users
   devise_for :admins
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
